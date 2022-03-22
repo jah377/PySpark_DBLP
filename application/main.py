f5ef4b294d60124cd4f5932a03ca2e1b83602625
Lucas Prieto <2324138p@student.gla.ac.uk>
2022-03-22 19:45:36 +0100

    small changes

--- application/main.py
+++ application/main.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from googletrans import Translator
 import pyspark.sql.functions as fn
+from pyspark.sql.types import ArrayType, FloatType, StringType
 import pyspark.sql.types as tp
 translator = Translator()
 def translate(c):
@@ -13,7 +14,10 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 
-to_eng = fn.udf(translate, fn.StringType())
+#to_eng = fn.udf(translate, StringType())
+def upperCase(str):
+    return str.upper()
+f = fn.udf(lambda x: "", StringType())
 
 train = spark.read.csv(f'application/data/train.csv')
 for i in range(1,5):
@@ -21,6 +25,7 @@ for i in range(1,5):
     df.withColumn('clean_author',fn.when(df.pauthor.endswith(".") & df.ptitle.contains('|'),df.ptitle).otherwise(df.pauthor))\
       .withColumn('clean_title',fn.when(df.pauthor.endswith(".") & df.ptitle.contains('|'),df.pauthor).otherwise(df.ptitle))\
       .withColumn('pyear', fn.abs(df.pyear))\
+      .select(f(fn.col('ptitle')).alias('translated'))\
       .drop(df.pauthor)\
       .drop(df.ptitle)\
       .show()
